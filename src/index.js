44f05db2a1509606a6e6cfdf7f72034f9c704a80
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-06-04 21:50:56 +0900

    redux 테스트

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from "react-dom/client";
-import App from "./App";
+// import React from "react";
+// // import ReactDOM from 'react-dom'; //구버전
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
+import { createStore } from "redux";
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const reducer = () => {
+  return "hello";
+}; //data를 수정하는 함수
+const store = createStore(); // data를 저장하는 공간
